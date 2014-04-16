5881afeac1f2d83214d0a52de2434de718064e0e
Planetguy <apricefrench2d@gmail.com>
2014-04-16 09:57:00 -0700

    Separated anonymous class into its own file, hopefully it shouldn't be hidden from Forge/MCP's renaming now

--- src/me/planetguy/remaininmotion/Items.java
+++ src/me/planetguy/remaininmotion/Items.java
@@ -1,29 +1,19 @@
 package me.planetguy.remaininmotion ;
 
-import java.util.List;
 
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
-import net.minecraft.world.World;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.TextureUtils;
-import codechicken.lib.vec.BlockCoord;
-import codechicken.lib.vec.Vector3;
 import codechicken.microblock.MicroMaterialRegistry.IMicroMaterial;
 import codechicken.microblock.MicroMaterialRegistry;
 import codechicken.microblock.MicroblockClass;
 import codechicken.microblock.MicroblockClassRegistry;
 import codechicken.microblock.MicroblockClient;
-import codechicken.multipart.JItemMultiPart;
-import codechicken.multipart.MultiPartRegistry;
-import codechicken.multipart.TMultiPart;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -44,38 +34,12 @@ public abstract class Items
 
 		SimpleItemSet = new SimpleItemSet ( ) ;
 
-		hollowCarriage=new JItemMultiPart(hollowCarriageId){
+		hollowCarriage=new FMPCarriageItem(hollowCarriageId);
 
-			@Override
-			public TMultiPart newPart(ItemStack arg0, EntityPlayer arg1,
-					World arg2, BlockCoord arg3, int arg4, Vector3 arg5) {
-				return MultiPartRegistry.createPart("FMPCarriage", false);
-			}
+		LanguageRegistry.addName(((net.minecraft.item.Item)hollowCarriage), "Hollow carriage");
 
-			@Override
-			public String getUnlocalizedName(){
-				return Mod.Handle+":"+"FMPCarriage";
-			}
-			
-			public String getItemDisplayName(ItemStack stack){
-				return "Multipart Carriage";
-			}
-
-			public Icon getIconFromDamage(int dmg){
-				return SimpleItemSet.getIconFromDamage(2);
-			}
-			
-		    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List l, boolean par4) {
-				if ( Configuration . Cosmetic . ShowHelpInTooltips )return;
-				l . add ( "Carries blocks directly touching it." ) ;
-				l.add("Supports Forge Multipart.");
-			}
-
-		};
-
-		LanguageRegistry.addName(hollowCarriage, "Hollow carriage");
-
-		hollowCarriage.setCreativeTab(CreativeTab.Instance);
+		//Attempting to fix FMP crashing when trying to set creative tab
+		((net.minecraft.item.Item)hollowCarriage).tabToDisplayOn=CreativeTab.Instance;
 
 	}
 }
