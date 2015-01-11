9a497b8a67600fd9fd13ceede7785361d14f9d75
Planetguy <apricefrench2d@gmail.com>
2015-01-11 10:08:56 -0800

    Fix mysteriously picking up FMP parts as if they're frame carriages

--- src/main/java/me/planetguy/remaininmotion/DefaultCarriageMatcher.java
+++ src/main/java/me/planetguy/remaininmotion/DefaultCarriageMatcher.java
@@ -2,6 +2,7 @@ package me.planetguy.remaininmotion;
 
 import me.planetguy.remaininmotion.api.CarriageMatcher;
 import me.planetguy.remaininmotion.api.Moveable;
+import me.planetguy.remaininmotion.carriage.TileEntityFrameCarriage;
 import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
 
@@ -9,7 +10,7 @@ public class DefaultCarriageMatcher implements CarriageMatcher {
 
 	@Override
 	public boolean matches(Block block1, int meta1, TileEntity entity1, Block bloc2k, int meta2, TileEntity entity2) {
-		return block1 == bloc2k && meta1 == meta2;
+		return entity1 instanceof TileEntityFrameCarriage && entity2 instanceof TileEntityFrameCarriage;
 	}
 
 	@Override
