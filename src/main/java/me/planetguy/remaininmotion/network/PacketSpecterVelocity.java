2859daccb186ce486d17e5577c630018ab492ce4
da3dsoul <da3dsoul@gmail.com>
2015-03-07 07:06:38 -0500

    Make the Specter Velocity Packet more reliable.

--- src/main/java/me/planetguy/remaininmotion/network/PacketSpecterVelocity.java
+++ src/main/java/me/planetguy/remaininmotion/network/PacketSpecterVelocity.java
@@ -4,22 +4,18 @@ import me.planetguy.remaininmotion.core.RiMConfiguration;
 import me.planetguy.remaininmotion.spectre.TileEntityMotiveSpectre;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-import java.util.Collection;
-
-/**
- * Created by Tom on 3/6/2015.
- */
 public abstract class PacketSpecterVelocity {
     public static void Dispatch(EntityPlayerMP Player) {
+        if(Player.worldObj.isRemote) return;
         NBTTagCompound Packet = new NBTTagCompound();
         Packet.setInteger("MotionDuration", RiMConfiguration.CarriageMotion.MotionDuration);
         PacketManager.SendPacketToPlayer(Player, PacketTypes.SpecterVelocity, Packet);
     }
 
     public static void Handle(NBTTagCompound Packet, World World) {
+        if(!World.isRemote) return;
         RiMConfiguration.CarriageMotion.MotionDuration = Packet.getInteger("MotionDuration");
         TileEntityMotiveSpectre.Velocity = 1 / ((double) RiMConfiguration.CarriageMotion.MotionDuration);
     }
