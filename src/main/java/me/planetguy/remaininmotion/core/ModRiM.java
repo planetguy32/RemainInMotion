23926385a5a1e50dcfae4fc9184e2bacf804c7a3
planetguy32 <apricefrench2d@gmail.com>
2015-04-27 21:16:30 -0700

    Bump version number for build 2.6.1

--- src/main/java/me/planetguy/remaininmotion/core/ModRiM.java
+++ src/main/java/me/planetguy/remaininmotion/core/ModRiM.java
@@ -28,7 +28,7 @@ public class ModRiM {
 
 	public static final String	Title		= "Remain In motion";
 
-	public static final String	Version		= "2.6.0";
+	public static final String	Version		= "2.6.1";
 
 	public static final String	Channel		= "JAKJ_RIM";
 
