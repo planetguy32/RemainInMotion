99b0287c60d5cd39ef0a6dd6ffbdbd0d585d6eab
planetguy32 <apricefrench2d@gmail.com>
2015-05-07 07:09:16 -0700

    Bump version

--- src/main/java/me/planetguy/remaininmotion/core/ModRiM.java
+++ src/main/java/me/planetguy/remaininmotion/core/ModRiM.java
@@ -32,7 +32,7 @@ public class ModRiM {
 
 	public static final String	Title		= "Remain In motion";
 
-	public static final String	Version		= "2.6.1";
+	public static final String	Version		= "2.7.0";
 
 	public static final String	Channel		= "JAKJ_RIM";
 
