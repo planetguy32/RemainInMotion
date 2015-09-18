ae74079487117c066675bbab5fac478713c276ec
Planetguy <apricefrench2d@gmail.com>
2015-09-18 07:58:23 -0700

    Add submodule and symlink to it

--- src/api/java/me/planetguy/lib/IComponent.java
+++
@@ -1,5 +0,0 @@
-package me.planetguy.lib;
-
-public interface IComponent {
-
-}
--- src/api/java/me/planetguy/lib/prefab/IPrefabBlock.java
+++
@@ -1,7 +0,0 @@
-package me.planetguy.lib.prefab;
-
-public interface IPrefabBlock extends IPrefabItem {
-
-	int countTooltipLines();
-
-}
