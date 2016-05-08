d6b6bfda7d2afea1cfa9421eb638e8a9eecc36bd
planetguy32 <apricefrench2d@gmail.com>
2016-05-08 15:18:20 -0700

    Looked briefly at BluePower rotation. It's just a start so far, but I've
    put my findings in a comment.

--- src/main/java/me/planetguy/remaininmotion/core/interop/mod/QmunityMultipart.java
+++ src/main/java/me/planetguy/remaininmotion/core/interop/mod/QmunityMultipart.java
@@ -10,12 +10,15 @@ import uk.co.qmunity.lib.part.compat.fmp.FMPPartFactory;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.ForgeDirection;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import me.planetguy.lib.util.Debug;
 import me.planetguy.lib.util.Reflection;
 import me.planetguy.lib.util.SneakyWorldUtil;
 import me.planetguy.remaininmotion.api.event.BlockSelectForMoveEvent;
+import me.planetguy.remaininmotion.api.event.RotatingTEPreUnpackEvent;
 import me.planetguy.remaininmotion.plugins.fmp.PartCarriageFMP;
 import me.planetguy.remaininmotion.util.position.BlockRecord;
 
@@ -62,6 +65,40 @@ public class QmunityMultipart implements Runnable {
 			}
 	}
 	
+	/*
+	@SubscribeEvent
+	public void transform(RotatingTEPreUnpackEvent e){
+        try {
+            NBTTagCompound tag=e.location.entityTag();
+            if(tag != null && tag.getString("id").equals("savedMultipart")) {
+            	NBTTagList parts=(NBTTagList) tag.getTagList("parts", 10);
+                for(int i=0; i<parts.tagCount(); i++) {
+                    NBTTagCompound part=parts.getCompoundTagAt(i);
+                    
+                    if(TODO part.getString("id").startsWith("pr")) {
+                    	transform(part, e.axis);
+                    }
+                }
+            }
+        }catch(Exception ignored) {}
+	}
+	
+	private void transform(NBTTagCompound part, ForgeDirection axis) {
+		//TODO
+		//AND gate
+		//{x:293,parts:[0:{data:{face:0,components:{0:{state:1b},1:{state:1b},2:{state:1b},3:{state:0b},4:{power:-1b,enabled:1b},5:{power:0b,enabled:1b},6:{power:0b,enabled:1b},7:{power:0b,enabled:1b},8:{}},rotation:2,buffer:{0_0:0b,0_1:0b,1_0:0b,1_1:0b,2_0:0b,2_1:0b,3_0:0b,3_1:0b,4_0:0b,4_1:0b},connections:{TOP:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:4},LEFT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:3},RIGHT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:1},FRONT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:0},BACK:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:2},BOTTOM:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:5}}},id:"0966688b-d390-4672-ae54-4c31fb8af0b1",type:"and"}],y:64,z:211,id:"qmunitylib.blocks.multipart"}
+		//{x:293,parts:[0:{data:{face:0,components:{0:{state:1b},1:{state:1b},2:{state:1b},3:{state:0b},4:{power:-1b,enabled:1b},5:{power:0b,enabled:1b},6:{power:0b,enabled:1b},7:{power:0b,enabled:1b},8:{}},rotation:2,buffer:{0_0:0b,0_1:0b,1_0:0b,1_1:0b,2_0:0b,2_1:0b,3_0:0b,3_1:0b,4_0:0b,4_1:0b},connections:{TOP:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:4},LEFT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:3},RIGHT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:1},FRONT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:0},BACK:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:2},BOTTOM:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:5}}},id:"0966688b-d390-4672-ae54-4c31fb8af0b1",type:"and"}],y:64,z:211,id:"qmunitylib.blocks.multipart"}
+		//{x:293,parts:[0:{data:{face:0,components:{0:{state:1b},1:{state:1b},2:{state:1b},3:{state:0b},4:{power:-1b,enabled:1b},5:{power:0b,enabled:1b},6:{power:0b,enabled:1b},7:{power:0b,enabled:1b},8:{}},rotation:1,buffer:{0_0:0b,0_1:0b,1_0:0b,1_1:0b,2_0:0b,2_1:0b,3_0:0b,3_1:0b,4_0:0b,4_1:0b},connections:{TOP:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:4},LEFT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:3},RIGHT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:1},FRONT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:0},BACK:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:2},BOTTOM:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:5}}},id:"e3ba30e1-8381-4405-9b0b-35225ebc2ea2",type:"and"}],y:64,z:211,id:"qmunitylib.blocks.multipart"}
+		//{x:293,parts:[0:{data:{face:0,components:{0:{state:1b},1:{state:1b},2:{state:1b},3:{state:0b},4:{power:-1b,enabled:1b},5:{power:0b,enabled:1b},6:{power:0b,enabled:1b},7:{power:0b,enabled:1b},8:{}},rotation:1,buffer:{0_0:0b,0_1:0b,1_0:0b,1_1:0b,2_0:0b,2_1:0b,3_0:0b,3_1:0b,4_0:0b,4_1:0b},connections:{TOP:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:4},LEFT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:3},RIGHT:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:1},FRONT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:0},BACK:{output:0b,input:0b,outputOnly:0b,enabled:1b,direction:2},BOTTOM:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:5}}},id:"e3ba30e1-8381-4405-9b0b-35225ebc2ea2",type:"and"}],y:64,z:211,id:"qmunitylib.blocks.multipart"}
+
+		//Sequencer
+		//{x:293,parts:[0:{data:{face:0,components:{0:{state:0b},1:{state:0b},2:{state:1b},3:{state:0b},4:{angle:0.2d,increment:0.00625d,state:1b},5:{}},ticks:0,rotation:0,start:634111L,time:160,connections:{TOP:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:4},LEFT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:3},RIGHT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:1},FRONT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:0},BACK:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:2},BOTTOM:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:5}}},id:"19d0bccb-30e9-44ab-9d51-a65f46e03995",type:"sequencer"}],y:64,z:212,id:"qmunitylib.blocks.multipart"}
+		//{x:293,parts:[0:{data:{face:0,components:{0:{state:0b},1:{state:0b},2:{state:1b},3:{state:0b},4:{angle:0.17500000000000002d,increment:0.00625d,state:1b},5:{}},ticks:0,rotation:0,start:634111L,time:160,connections:{TOP:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:4},LEFT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:3},RIGHT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:1},FRONT:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:0},BACK:{output:0b,input:0b,outputOnly:1b,enabled:1b,direction:2},BOTTOM:{output:0b,input:0b,outputOnly:0b,enabled:0b,direction:5}}},id:"19d0bccb-30e9-44ab-9d51-a65f46e03995",type:"sequencer"}],y:64,z:212,id:"qmunitylib.blocks.multipart"}
+
+	}
+	
+	*/
+
 	private final class QmunityConversionHelper extends TMultiPart {
 		@Override
 		public String getType() {
