a3f67c6a8258cd86ab254407157215d3494e2fb0
planetguy32 <apricefrench2d@gmail.com>
2016-05-07 20:30:10 -0700

    Handle Project:Red logic tiles correctly(?) - #189
    I haven't exhaustively tested this, but I think it puts things in the
    right orientations. I'm not sure if it's an artifact of my test
    structure, but it might be messing up the parts of the logic tile. Let
    me know if anything fails to rotate properly (I need before and after
    orientations to find the table entry that's wrong).

--- src/main/java/me/planetguy/remaininmotion/core/interop/mod/ProjectRed.java
+++ src/main/java/me/planetguy/remaininmotion/core/interop/mod/ProjectRed.java
@@ -1,9 +1,11 @@
 package me.planetguy.remaininmotion.core.interop.mod;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import me.planetguy.lib.util.Debug;
 import me.planetguy.remaininmotion.api.event.RotatingTEPreUnpackEvent;
 import me.planetguy.remaininmotion.util.transformations.Rotator;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class ProjectRed {
 
@@ -14,14 +16,32 @@ public class ProjectRed {
             if(tag != null && tag.getString("id").equals("savedMultipart")) {
                 for(int i=0; i<tag.getTagList("parts", 10).tagCount(); i++) {
                     NBTTagCompound part=tag.getTagList("parts", 10).getCompoundTagAt(i);
-                    if(part.getString("id").startsWith("pr") && part.hasKey("side")) {
-                        int side = part.getByte("side");
-                        side = Rotator.newSide(side, e.axis);
-                        part.setByte("side", (byte)side);
+                    if(part.getString("id").startsWith("pr")) {
+                    	transform(part, e.axis);
                     }
                 }
             }
         }catch(Exception ignored) {}
     }
+
+	private void transform(NBTTagCompound part, ForgeDirection axis) {
+		if(part.hasKey("side")) {
+            int side = part.getByte("side");
+            side = Rotator.newSide(side, axis);
+            part.setByte("side", (byte)side);
+		} else if(part.hasKey("orient")) {
+ 			int orient=part.getByte("orient");
+			try{
+				orient=Rotator.newLogicTileOrientation(orient, axis);
+				if(orient!=-1)
+					part.setByte("orient", (byte) orient);
+				else
+					Debug.mark();
+			}catch(ArrayIndexOutOfBoundsException e){
+				
+			}
+		}
+		
+	}
 
 }
--- src/main/java/me/planetguy/remaininmotion/util/transformations/Rotator.java
+++ src/main/java/me/planetguy/remaininmotion/util/transformations/Rotator.java
@@ -23,4 +23,40 @@ public class Rotator {
 		return newSide(side, axis.ordinal());
 	}
 
+	//Good for Project:Red logic tiles
+	public static int newLogicTileOrientation(int orient, ForgeDirection axis) {
+		int[][] table=new int[][]{
+			//This is the meat of this function.
+			//Every odd table is derived from the preceding even table by reversing it (due to the nature of rotation).
+				{3, 0, 1, 2, 5, 6, 7, 4,
+				16,17,18,19,20,21,22,23,
+				12,13,14,15, 8, 9,10,11},
+				
+				{1, 2, 3, 0, 7, 4, 5, 6,
+				20,21,22,23,16,17,18,19,
+				 8, 9,10,11,12,13,14,15},
+				
+				{21,22,23,24,19,16,17,18,
+				 11, 8, 9,10,15,12,13,14,
+				  1, 2, 3, 0, 7, 4, 5, 6},
+				
+				//TODO
+				{19,16,17,18,21,22,23,20,
+				  9,10,11, 8,13,14,15,12,
+				  5, 6, 7, 4, 0, 1, 2, 3
+				},
+				
+				{10,11, 8, 9,14,15,12,13,
+				  4, 5, 6, 7, 0, 1, 2, 3,
+				 19,18,17,16,21,22,23,20
+				},
+				
+				{12,13,14,15, 8, 9,10,11,
+				  2, 3, 0, 1, 6, 7, 4, 5,
+				 19,18,17,16,23,20,21,22
+				}
+		};
+		return table[axis.ordinal()][orient];
+	}
+
 }
